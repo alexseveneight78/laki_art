370ba63d99ebf7d3b331d1d5fd14b5b2d65e3110
alexseveneight <alexseveneight@gmail.com>
2021-02-17 22:02:22 +0300

    'structure'

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      hello
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
